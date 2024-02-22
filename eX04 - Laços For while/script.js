ae2d67f4d1a5f619140084b083a2da9a22efe5dc
Priscilla Alves <perealvees@gmail.com>
2024-02-22 16:48:46 -0300

    for e while

---
+++ eX04 - Laços For while/script.js
@@ -0,0 +1,20 @@
+//- Crie exercícios de repetição, onde aparece uma lista de números de 1 a 1000
+//usando o for e while 
+
+
+/*for (var num2 = 25; num2 <500; num2 += 2){ // aqui começa no número 5 e pula de 2 em 2
+    console.log(num2);
+}
+
+for (var num = 0; num < 1000; num++ ){ // aqui começa no número 0 e pula de 1 em 1
+    console.log(num)
+} */
+
+
+var num = 2
+while(num <= 1000){
+    console.log(num)
+    num += 2
+}
+
+
